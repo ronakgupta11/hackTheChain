0c434034c0ce13f25216ab6b9d1c9b8ec3fdd1e7
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-03-25 00:02:11 +0530

    package setup

---
+++ new-app/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+import './App.css'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+
+function App() {
+const network = WalletAdapterNetwork.Devnet;
+const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+const wallets = useMemo(
+  () => [
+    new PhantomWalletAdapter(),
+    new GlowWalletAdapter()
+  ],
+  [network]
+);
+
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <div className='app'>
+            <WalletMultiButton/>
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+}
+
+export default App
